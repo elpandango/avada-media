0c93f78322eb9b940c200fd48b5bcb108442b503
Pandango <Pandango>
2017-03-13 01:38:46 +0200

    Commit #6

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
     var button = $('.btn-extended'),
         height = 0,
         article = '',
-        arrow = '';
+        arrow = '',
+        triggerHead = $('.trigger').find('.trigger-head'),
+        toTop = $('#to-top'),
+        navbar = $('header.menu'),
+        dropdown = navbar.find('.dropdown');
+
 
     button.on('click', '.text', function () {
         article = $(this).closest('.container').find('.hid-text');
@@ -20,6 +25,52 @@ $(document).ready(function () {
                 height: 150
             }, 500);
         }
+    });
+
+    triggerHead.on('click', function (e) {
+        e.stopPropagation();
+        $(this).closest('.trigger').toggleClass('active');
+    });
+
+    //window.onscroll = function() {
+    $(window).on('scroll', function () {
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled >= 250) {
+            toTop.show();
+        } else {
+            toTop.hide();
+        }
+    });
+
+    toTop.on('click', function () {
+        $("body,html").stop().animate({
+            scrollTop:0
+        }, 500);
+    });
+
+    $('#burger').on('click', function () {
+        navbar.toggleClass('active');
+        if (navbar.hasClass('active')) {
+            dropdown.slideDown(500);
+        } else {
+            dropdown.slideUp(500);
+        }
+    });
+
+    $('.scroll').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+
+        navbar.removeClass('active');
+    });
+
+    $('.pop-img').on('click', function(){
+        var bg = $(this).css('background-image');
+        bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+
+        $('#myModal').find('.modal-image').attr('src', bg);
     });
 
 });
